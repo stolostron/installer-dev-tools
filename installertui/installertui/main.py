5b2c80303e6966178bc74799269f13bc58952f5e
Nathaniel Graham <nathaniel.graham@protonmail.com>
2024-01-05 18:32:28 -0700

    searches projects for deployments

--- installertui/installertui/main.py
+++ installertui/installertui/main.py
@@ -1,11 +1,28 @@
 import openshift as oc
 
-def main():
-    with oc.tracking() as _, oc.timeout(10*60):
-        print('OpenShift client version: {}'.format(oc.get_client_version()))
+def filtered_projects(projects):
+    filtered = []
+    for project in projects:
+        if "openshift" in project:
+                continue
+        if "kube" in project:
+            continue
+        if "default" in project:
+            continue
+        filtered.append(project)
+    return filtered
 
+def main():
+    with oc.tracking() as _:
         project_selector = oc.selector("projects")
-        print("Project names: " + str(project_selector.qnames()))
+        projects = filtered_projects(project_selector.names())
+        for project in projects:
+            print("-------------------")
+            print("Project: " + project)
+            with oc.project(project):
+                dep_selector = oc.selector("deployments")
+                print(dep_selector.qnames())
+        print()
         
 if __name__ == "__main__":
     main()
