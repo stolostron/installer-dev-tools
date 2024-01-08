01a953788d4b9cde9d90caa19c85cbce840e674e
Nathaniel Graham <[nathaniel.graham@protonmail.com, ngraham@redhat.com]>
2024-01-08 10:14:27 -0700

    removed extraneous code

--- installertui/installertui/main.py
+++ installertui/installertui/main.py
@@ -1,13 +1,5 @@
 import openshift as oc
 
-def filtered_projects(projects):
-    filtered = []
-    for project in projects:
-        if project != "multicluster-engine" and project != "open-cluster-management":
-            continue
-        filtered.append(project)
-    return filtered
-
 def main():
     with oc.tracking() as _:
         with oc.project("multicluster-engine"):
@@ -34,27 +26,6 @@ def main():
                     print(f"Release version mismatch. ACM: {acm_version}, Annotation: {release_version}")
                 else:
                     print(f"Release version match. ACM: {acm_version}, Annotation: {release_version}")
-
-        
-
-        # project_selector = oc.selector("projects")
-        # projects = filtered_projects(project_selector.names())
-        # for project in projects:
-        #     print("-------------------")
-        #     print("Project: " + project)
-        #     with oc.project(project):
-        #         dep_selector = oc.selector("deployments")
-        #         print(dep_selector.names())
-        # print()
-        # with oc.project(projects[0]):
-        #     op_selector = oc.selector("csv")
-        #     print(op_selector.objects()[0].model.spec.version)
-
-        #     dep_selector = oc.selector("deployments")
-        #     deployments = dep_selector.objects()
-        #     deployment = deployments[0]
-        #     print(f"The project is: {deployment.kind()}/{deployment.name()}")
-        #     print(f"With annotations: {deployment.model.metadata.annotations}")
         
 if __name__ == "__main__":
     main()
