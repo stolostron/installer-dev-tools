283665f96e95722b9e6535219ce55efe5cb887de
Nathaniel Graham <[nathaniel.graham@protonmail.com, ngraham@redhat.com]>
2024-01-15 16:56:06 -0700

    gathers components from a list now

--- release-version.sh
+++ release-version.sh
@@ -12,6 +12,10 @@ record_version_match_status() {
     echo "$1 $2 $3 $4 $5 $(version_matches $4 $5)" >> version-matches
 }
 
+clear_version_match_file() {
+    echo "" > version-matches
+}
+
 version_matches() {
     local component_version=$1
     local annotation_version=$2
@@ -22,35 +26,53 @@ version_matches() {
     fi
 }
 
-acm_version=$(oc get csv -n open-cluster-management -oyaml | yq '.items[0].spec.version')
-mce_version=$(oc get csv -n multicluster-engine -oyaml | yq '.items[0].spec.version')
-echo "" > version-matches
+scan_components() {
+    local kinds=("deployment")
 
-acm_deployments=$(oc get deployments -n open-cluster-management | awk 'NR>1 {print $1}')
-mce_deployments=$(oc get deployments -n multicluster-engine | awk 'NR>1 {print $1}')
+    local acm_version=$(oc get csv -n open-cluster-management -oyaml | yq '.items[0].spec.version')
+    local mce_version=$(oc get csv -n multicluster-engine -oyaml | yq '.items[0].spec.version')
 
-for deployment in $mce_deployments; do
-    release_version=$(oc get deployment -n multicluster-engine $deployment -oyaml | yq '.metadata.annotations["installer.multicluster.openshift.io/release-version"]')
-    echo "--------------------"
-    echo "Multicluster Engine"
-    echo "Deployment: $deployment"
-    if [[ "$release_version" == "$mce_version" ]]; then
-        echo "Release verions match: $release_version"
+    for kind in $kinds; do
+        local acm_resources=$(oc get $kind -n open-cluster-management | awk 'NR>1 {print $1}')
+        local mce_resources=$(oc get $kind -n multicluster-engine | awk 'NR>1 {print $1}')
+
+        for resource in $mce_resources; do
+            release_version=$(oc get $kind -n multicluster-engine $resource -oyaml | yq '.metadata.annotations["installer.multicluster.openshift.io/release-version"]')
+            echo "--------------------"
+            echo "Multicluster Engine"
+            echo "$kind: $resource"
+            if [[ "$release_version" == "$mce_version" ]]; then
+                echo "Release verions match: $release_version"
+            else
+                echo "Release version mismatch. MCE: $mce_version, Annotation: $release_version"
+            fi
+            record_version_match_status "mce" "$kind" $resource $mce_version $release_version
+        done
+
+        for resource in $acm_resources; do
+            release_version=$(oc get $kind -n open-cluster-management $resource -oyaml | yq '.metadata.annotations["installer.open-cluster-management.io/release-version"]')
+            echo "--------------------"
+            echo "Open Cluster Management"
+            echo "$kind: $resource"
+            if [[ "$release_version" == "$acm_version" ]]; then
+                echo "Release versions match: $release_version"
+            else
+                echo "Release version mismatch. ACM: $acm_version, Annotation: $release_version"
+            fi
+            record_version_match_status "acm" "$kind" $resource $acm_version $release_version
+        done
+    done
+}
+
+compare_versions() {
+    local operator_version=$1
+    local component_version=$2
+    if [[ "$component_version" == "$operator_version" ]]; then
+        true
     else
-        echo "Release version mismatch. MCE: $mce_version, Annotation: $release_version"
+        false
     fi
-    record_version_match_status "mce" "deployment" $deployment $mce_version $release_version
-done
+}
 
-for deployment in $acm_deployments; do
-    release_version=$(oc get deployment -n open-cluster-management $deployment -oyaml | yq '.metadata.annotations["installer.open-cluster-management.io/release-version"]')
-    echo "--------------------"
-    echo "Open Cluster Management"
-    echo "Deployment: $deployment"
-    if [[ "$release_version" == "$acm_version" ]]; then
-        echo "Release versions match: $release_version"
-    else
-        echo "Release version mismatch. MCE: $mce_version, Annotation: $release_version"
-    fi
-    record_version_match_status "acm" "deployment" $deployment $mce_version $release_version
-done
+clear_version_match_file
+scan_components
