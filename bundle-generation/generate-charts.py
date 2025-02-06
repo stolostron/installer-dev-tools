dec75797fef1ed691b7d4eadbb5c6a340d5e7c2b
Cameron Wall <cwall@redhat.com>
2025-02-06 12:01:59 -0500

    job add
    Signed-off-by: Cameron Wall <cwall@redhat.com>

--- bundle-generation/generate-charts.py
+++ bundle-generation/generate-charts.py
@@ -796,7 +796,7 @@ def update_helm_resources(chartName, helmChart, skip_rbac_overrides, exclusions,
                         resource_data['metadata']['name'] = 'flightctl-api-{{ .Values.global.namespace }}'
 
                     if kind == "NetworkPolicy":
-                        # resource_data['metadata']['namespace'] = '{{ .Values.global.namespace }}'
+                        resource_data['metadata']['namespace'] = '{{ .Values.global.namespace }}'
                         new_values = ["{{ .Values.global.namespace }}", "openshift-console"]
                         resource_data['spec']['ingress'][0]['from'][0]['namespaceSelector']['matchExpressions'][0]['values'] = new_values
                     if kind == "Job":
