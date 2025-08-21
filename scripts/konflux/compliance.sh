b43b457c90c5e5c4010ccb29854701dfc29425e9
Nathaniel Graham <ngraham@redhat.com>
2025-08-21 15:07:39 -0600

    updated
    Signed-off-by: Nathaniel Graham <ngraham@redhat.com>

--- scripts/konflux/compliance.sh
+++ scripts/konflux/compliance.sh
@@ -65,22 +65,59 @@ check_hermetic_builds() {
     local authorization="$3"
     local org="$4"
     local repo="$5"
-    local yaml_base="$6"
-    local value="$7"
     
     hermeticbuilds=true
-    pathinrepo=$(echo "$yaml" | yq "$yaml_base.pipelineRef.params | .[] | select(.name==\"pathInRepo\")")
-    pullpathinrepo=$(echo "$pull_yaml" | yq "$yaml_base.pipelineRef.params | .[] | select(.name==\"pathInRepo\")")
+    
+    # Check pathInRepo (first .spec, then fallback to .spec.pipelineSpec)
+    pathinrepo=$(echo "$yaml" | yq ".spec.pipelineRef.params | .[] | select(.name==\"pathInRepo\")")
+    [[ -n "$debug" ]] && echo "[debug] pathInRepo (push): using .spec = $pathinrepo" >&3
+    if [[ -z "$pathinrepo" ]]; then
+        pathinrepo=$(echo "$yaml" | yq ".spec.pipelineSpec.pipelineRef.params | .[] | select(.name==\"pathInRepo\")")
+        [[ -n "$debug" ]] && echo "[debug] pathInRepo (push): using .spec.pipelineSpec fallback = $pathinrepo" >&3
+    fi
+    
+    pullpathinrepo=$(echo "$pull_yaml" | yq ".spec.pipelineRef.params | .[] | select(.name==\"pathInRepo\")")
+    [[ -n "$debug" ]] && echo "[debug] pathInRepo (pull): using .spec = $pullpathinrepo" >&3
+    if [[ -z "$pullpathinrepo" ]]; then
+        pullpathinrepo=$(echo "$pull_yaml" | yq ".spec.pipelineSpec.pipelineRef.params | .[] | select(.name==\"pathInRepo\")")
+        [[ -n "$debug" ]] && echo "[debug] pathInRepo (pull): using .spec.pipelineSpec fallback = $pullpathinrepo" >&3
+    fi
 
     if [[ -z "$pathinrepo" || -z "$pullpathinrepo" ]]; then
-        buildsourceimage=$(echo "$yaml" | yq "$yaml_base.params | .[] | select(.name==\"build-source-image\") | $value")
-        pull_bsi=$(echo "$pull_yaml" | yq "$yaml_base.params | .[] | select(.name==\"build-source-image\") | $value")
+        # Check build-source-image (first .spec.params.value, then fallback to .spec.pipelineSpec.params.default)
+        buildsourceimage=$(echo "$yaml" | yq ".spec.params | .[] | select(.name==\"build-source-image\") | .value")
+        [[ -n "$debug" ]] && echo "[debug] build-source-image (push): using .spec.params.value = $buildsourceimage" >&3
+        if [[ -z "$buildsourceimage" ]]; then
+            buildsourceimage=$(echo "$yaml" | yq ".spec.pipelineSpec.params | .[] | select(.name==\"build-source-image\") | .default")
+            [[ -n "$debug" ]] && echo "[debug] build-source-image (push): using .spec.pipelineSpec.params.default = $buildsourceimage" >&3
+        fi
+        
+        pull_bsi=$(echo "$pull_yaml" | yq ".spec.params | .[] | select(.name==\"build-source-image\") | .value")
+        [[ -n "$debug" ]] && echo "[debug] build-source-image (pull): using .spec.params.value = $pull_bsi" >&3
+        if [[ -z "$pull_bsi" ]]; then
+            pull_bsi=$(echo "$pull_yaml" | yq ".spec.pipelineSpec.params | .[] | select(.name==\"build-source-image\") | .default")
+            [[ -n "$debug" ]] && echo "[debug] build-source-image (pull): using .spec.pipelineSpec.params.default = $pull_bsi" >&3
+        fi
+        
         if [[ !($buildsourceimage == true || $buildsourceimage == "true") || !($pull_bsi == true || $pull_bse == "true") ]]; then
             hermeticbuilds=false
         fi
 
-        hermetic=$(echo "$yaml" | yq "$yaml_base.params | .[] | select(.name==\"hermetic\") | $value")
-        pull_hermetic=$(echo "$pull_yaml" | yq "$yaml_base.params | .[] | select(.name==\"hermetic\") | $value")
+        # Check hermetic (first .spec.params.value, then fallback to .spec.pipelineSpec.params.default)
+        hermetic=$(echo "$yaml" | yq ".spec.params | .[] | select(.name==\"hermetic\") | .value")
+        [[ -n "$debug" ]] && echo "[debug] hermetic (push): using .spec.params.value = $hermetic" >&3
+        if [[ -z "$hermetic" ]]; then
+            hermetic=$(echo "$yaml" | yq ".spec.pipelineSpec.params | .[] | select(.name==\"hermetic\") | .default")
+            [[ -n "$debug" ]] && echo "[debug] hermetic (push): using .spec.pipelineSpec.params.default = $hermetic" >&3
+        fi
+        
+        pull_hermetic=$(echo "$pull_yaml" | yq ".spec.params | .[] | select(.name==\"hermetic\") | .value")
+        [[ -n "$debug" ]] && echo "[debug] hermetic (pull): using .spec.params.value = $pull_hermetic" >&3
+        if [[ -z "$pull_hermetic" ]]; then
+            pull_hermetic=$(echo "$pull_yaml" | yq ".spec.pipelineSpec.params | .[] | select(.name==\"hermetic\") | .default")
+            [[ -n "$debug" ]] && echo "[debug] hermetic (pull): using .spec.pipelineSpec.params.default = $pull_hermetic" >&3
+        fi
+        
         if [[ $hermetic != true || $hermetic != "true" || $pull_hermetic != true || $pull_hermetic != "true" ]]; then
             hermeticbuilds=false
         fi
@@ -88,8 +125,22 @@ check_hermetic_builds() {
 
     vendor=$(curl -LsH "$authorization" -w "%{http_code}" "https://api.github.com/repos/$org/$repo/contents/vendor")
     vendor="${vendor: -3}"
-    prefetch=$(echo "$yaml" | yq "$yaml_base.params | .[] | select(.name==\"prefetch-input\") | $value")
-    pull_prefetch=$(echo "$pull_yaml" | yq "$yaml_base.params | .[] | select(.name==\"prefetch-input\") | $value")
+    
+    # Check prefetch-input (first .spec.params.value, then fallback to .spec.pipelineSpec.params.default)
+    prefetch=$(echo "$yaml" | yq ".spec.params | .[] | select(.name==\"prefetch-input\") | .value")
+    [[ -n "$debug" ]] && echo "[debug] prefetch-input (push): using .spec.params.value = $prefetch" >&3
+    if [[ -z "$prefetch" ]]; then
+        prefetch=$(echo "$yaml" | yq ".spec.pipelineSpec.params | .[] | select(.name==\"prefetch-input\") | .default")
+        [[ -n "$debug" ]] && echo "[debug] prefetch-input (push): using .spec.pipelineSpec.params.default = $prefetch" >&3
+    fi
+    
+    pull_prefetch=$(echo "$pull_yaml" | yq ".spec.params | .[] | select(.name==\"prefetch-input\") | .value")
+    [[ -n "$debug" ]] && echo "[debug] prefetch-input (pull): using .spec.params.value = $pull_prefetch" >&3
+    if [[ -z "$pull_prefetch" ]]; then
+        pull_prefetch=$(echo "$pull_yaml" | yq ".spec.pipelineSpec.params | .[] | select(.name==\"prefetch-input\") | .default")
+        [[ -n "$debug" ]] && echo "[debug] prefetch-input (pull): using .spec.pipelineSpec.params.default = $pull_prefetch" >&3
+    fi
+    
     # echo "$prefetch $pull_prefetch"
     # echo -e "Prefetch: $prefetch\nPullPrefetch: $pull_prefetch\nVendor: $vendor"
     if [[ ($prefetch == "" || $pull_prefetch == "") && $vendor != "200" ]]; then
@@ -101,7 +152,7 @@ check_hermetic_builds() {
         echo "🟩 $repo hermetic builds: TRUE" >&3
         echo "Enabled"
     else
-        echo -e "🟥 $repo hermetic builds: FALSE\nbuild-source-image: $buildsourceimage\npull_hermetic: $pull_hermetic\npush_hermetic: $hermetic\npull_prefetch: $pull_prefetch\npush_prefetch: $prefetch\nvendor: $vendor" >&3
+        echo "🟥 $repo hermetic builds: FALSE" >&3
         echo "Not Enabled"
     fi
 }
@@ -141,10 +192,14 @@ check_enterprise_contract() {
 check_multiarch_support() {
     local yaml="$1"
     local repo="$2"
-    local yaml_base="$3"
-    local value="$4"
     
-    platforms=$(echo "$yaml" | yq "$yaml_base.params | .[] | select(.name==\"build-platforms\") | $value | .[]" | wc -l | tr -d ' \t\n')
+    # Check build-platforms (first .spec.params.value, then fallback to .spec.pipelineSpec.params.default)
+    platforms_value=$(echo "$yaml" | yq ".spec.params | .[] | select(.name==\"build-platforms\") | .value | .[]")
+    if [[ -z "$platforms_value" ]]; then
+        platforms_value=$(echo "$yaml" | yq ".spec.pipelineSpec.params | .[] | select(.name==\"build-platforms\") | .default | .[]")
+    fi
+    
+    platforms=$(echo "$platforms_value" | wc -l | tr -d ' \t\n')
     if  [[ $platforms != 4 ]]; then
         echo "🟥 $repo Multiarch: FALSE" >&3
         echo "Not Enabled"
@@ -154,7 +209,7 @@ check_multiarch_support() {
     fi
 }
 
-if [[ -n "$debug" ]]; then
+if [[ -n "$debug" && "$debug" != "true" ]]; then
     components=$debug
 else
     components=$(oc get components | grep $application | awk '{print $1}')
@@ -182,19 +237,11 @@ for line in $components; do
     yaml=$(curl -Ls $push)
     pull_yaml=$(curl -Ls $push)
 
-    hermetic_result=$(check_hermetic_builds "$yaml" "$pull_yaml" "$authorization" "$org" "$repo" ".spec" ".value")
-    if [[ "$hermetic_result" == "Not Enabled" ]]; then
-        hermetic_result=$(check_hermetic_builds "$yaml" "$pull_yaml" "$authorization" "$org" "$repo" ".spec.pipelineSpec" ".default")
-    fi
-    data="$data,$hermetic_result"
+    data="$data,$(check_hermetic_builds "$yaml" "$pull_yaml" "$authorization" "$org" "$repo")"
 
     data="$data,$(check_enterprise_contract "$application" "$line" "$authorization" "$org" "$repo" "$branch")"
 
-    multiarch_result=$(check_multiarch_support "$yaml" "$repo" ".spec" ".value")
-    if [[ "$multiarch_result" == "Not Enabled" ]]; then
-        multiarch_result=$(check_multiarch_support "$yaml" "$repo" ".spec.pipelineSpec" ".default")
-    fi
-    data="$data,$multiarch_result"
+    data="$data,$(check_multiarch_support "$yaml" "$repo")"
 
     echo ""
 
