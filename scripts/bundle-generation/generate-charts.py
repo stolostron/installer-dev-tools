1b1c031a15c5a5d1d8a3a1f8acbc0b6c86563cc3
Cameron Wall <cwall@redhat.com>
2025-09-16 16:12:34 -0400

    edge management image 2.15
    Signed-off-by: Cameron Wall <cwall@redhat.com>

--- scripts/bundle-generation/generate-charts.py
+++ scripts/bundle-generation/generate-charts.py
@@ -469,6 +469,18 @@ def fixImageReferences(helmChart, imageKeyMapping):
                 container['image'] = "{{ .Values.global.imageOverrides." + image_key + " }}"
                 container['imagePullPolicy'] = "{{ .Values.global.pullPolicy }}"
 
+            containers = resource_data['spec']['template']['spec']['initContainers']
+            for container in containers:
+                image_key = parse_image_ref(container['image'])["repository"]
+                try:
+                    image_key = imageKeyMapping[image_key]
+                except KeyError:
+                    logging.critical("No image key mapping provided for imageKey: %s" % image_key)
+                    exit(1)
+                imageKeys.append(image_key)
+                container['image'] = "{{ .Values.global.imageOverrides." + image_key + " }}"
+                container['imagePullPolicy'] = "{{ .Values.global.pullPolicy }}"
+
                 if kind == "Deployment":
                     args = container.get('args', [])
                     refreshed_args = []
