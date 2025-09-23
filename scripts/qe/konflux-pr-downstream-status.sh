0b1df88f70f6ef03b88d06809d08b3abfde96b8d
Nathaniel Graham <ngraham@redhat.com>
2025-09-23 16:37:26 -0600

    Konflux PR status now works for multiple PRs
    Signed-off-by: Nathaniel Graham <ngraham@redhat.com>

--- scripts/qe/konflux-pr-downstream-status.sh
+++ scripts/qe/konflux-pr-downstream-status.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+# Set up file descriptor 3 for output messages
+exec 3>&1
+
 declare -A repo_commits
+
+# Debug output function
+debug_echo() {
+  if [ "$debug" = true ]; then
+    echo "$@" >&3
+  fi
+}
 
 # Help function
 show_help() {
   cat << EOF
-Usage: $0 -a|--application <application> -t|--tag <tag> [PR_URL1] [PR_URL2] ...
+Usage: $0 -a|--application <application> -t|--tag <tag> [--debug] [PR_URL1] [PR_URL2] ...
 
 This script checks if PRs have made it into the latest downstream build image.
 
 Options:
   -a, --application <app>    Application name (e.g., acm-215, acm-214)
   -t, --tag <tag>           Tag name (e.g., latest-2.15)
+  --debug                   Enable debug output
   -h, --help                Show this help message
 
 Arguments:
@@ -19,12 +30,13 @@ Arguments:
 
 Example:
   $0 -a acm-215 -t latest-2.15 https://github.com/stolostron/multiclusterhub-operator/pull/2668
-  $0 --application acm-214 --tag latest-2.14 https://github.com/org/repo/pull/123 https://github.com/org/repo/pull/456
+  $0 --application acm-214 --tag latest-2.14 --debug https://github.com/org/repo/pull/123
 EOF
 }
 
 # Parse command line arguments
 pr_urls=()
+debug=false
 while [[ $# -gt 0 ]]; do
   case $1 in
     -a|--application)
@@ -34,6 +46,10 @@ while [[ $# -gt 0 ]]; do
     -t|--tag)
       tag="$2"
       shift 2
+      ;;
+    --debug)
+      debug=true
+      shift
       ;;
     -h|--help)
       show_help
@@ -69,54 +85,71 @@ if [ ${#pr_urls[@]} -eq 0 ]; then
   exit 1
 fi
 
+# Parse application and set up common variables
 application_part=$(echo "$application" | cut -d'-' -f1)
 version_number=$(echo "$application" | cut -d'-' -f2)
 major_version=$(echo "$version_number" | cut -c1)
 minor_version=$(echo "$version_number" | cut -c2-)
-branch="release-$major_version.$minor_version"
-latest_snapshot_url="https://raw.githubusercontent.com/stolostron/$application_part-operator-bundle/refs/heads/$branch/latest-snapshot.yaml"
-pr="https://github.com/stolostron/multiclusterhub-operator/pull/2668"
-repo=$(dirname $(dirname $pr))
-repo_name=$(basename $repo)
 
+# Handle different application types
+if [ "$application_part" = "mce" ]; then
+  branch="backplane-$major_version.$minor_version"
+  channel="stable-$major_version.$minor_version"
+else
+  branch="release-$major_version.$minor_version"
+  channel="$branch"
+fi
+
+latest_snapshot_url="https://raw.githubusercontent.com/stolostron/$application_part-operator-bundle/refs/heads/$branch/latest-snapshot.yaml"
 gen_config_url="https://raw.githubusercontent.com/stolostron/$application_part-operator-bundle/refs/heads/$branch/config/$application_part-manifest-gen-config.json"
 
-echo "Application: $application_part"
-echo "Branch: $branch"
-echo "PR: $pr"
-echo "Repo: $repo"
-catalog="quay.io/acm-d/acm-dev-catalog:$tag"
-
-# opm render $catalog -oyaml > $tag.cs.yaml
-csv=$(cat $tag.cs.yaml | yq "select(.schema == \"olm.channel\" and .name == \"$branch\") | .entries[-1].name")
-echo "CSV: $csv"
-
-publish_name=$(curl -Ls "$gen_config_url" | yq -p=json ".product-images.image-list[] | select(.konflux-component-name == \"$repo_name\") | .publish-name")
-echo "Image Name: $publish_name"
-
-published_image=$(cat $tag.cs.yaml | yq "select(.schema == \"olm.bundle\" and .name == \"$csv\") | .relatedImages[] | select(.image==\"*$publish_name*\") | .image")
-echo "Published Image: $published_image"
-published_image_sha=$(basename $published_image | cut -d'@' -f2)
-echo "Published Image SHA: $published_image_sha"
-
-component="$repo_name-acm-215"
-
-calculated_image="$component@$published_image_sha"
-echo "Calculated Image: $calculated_image"
-
-revision_by_sha=$(curl -Ls "$latest_snapshot_url" | yq ".spec.components[] | select(.containerImage == \"*$calculated_image\") | .source.git.revision")
-echo "Revision by SHA: $revision_by_sha"
-
-revision_by_repo=$(curl -Ls "$latest_snapshot_url" | yq ".spec.components[] | select(.source.git.url == \"$repo\") | .source.git.revision")
-echo "Revision by Repo: $revision_by_repo"
-
-
-echo "Checking for Github auth token"
+debug_echo "Checking for Github auth token"
 authorization=""
 if [ -f "authorization.txt" ]; then
 	authorization="Authorization: Bearer $(cat "authorization.txt")"
-	echo "Authorization found. Applying to github API requests"
+	debug_echo "Authorization found. Applying to github API requests"
 fi
+
+catalog="quay.io/acm-d/$application_part-dev-catalog:$tag"
+
+# opm render $catalog -oyaml > $tag.cs.yaml
+
+function get_revision_for_pr {
+  local pr_url="$1"
+  local repo=$(dirname $(dirname $pr_url))
+  local repo_name=$(basename $repo)
+
+  debug_echo "Application: $application_part"
+  debug_echo "Branch: $branch"
+  debug_echo "Channel: $channel"
+  debug_echo "PR: $pr_url"
+  debug_echo "Repo: $repo"
+
+  local csv=$(cat $tag.cs.yaml | yq "select(.schema == \"olm.channel\" and .name == \"$channel\") | .entries[-1].name")
+  debug_echo "CSV: $csv"
+
+  local publish_name=$(curl -Ls "$gen_config_url" | yq -p=json ".product-images.image-list[] | select(.konflux-component-name == \"$repo_name\") | .publish-name")
+  debug_echo "Image Name: $publish_name"
+
+  local published_image=$(cat $tag.cs.yaml | yq "select(.schema == \"olm.bundle\" and .name == \"$csv\") | .relatedImages[] | select(.image==\"*$publish_name*\") | .image")
+  debug_echo "Published Image: $published_image"
+  local published_image_sha=$(basename $published_image | cut -d'@' -f2)
+  debug_echo "Published Image SHA: $published_image_sha"
+
+  local component="$repo_name-$application"
+
+  local calculated_image="$component@$published_image_sha"
+  debug_echo "Calculated Image: $calculated_image"
+
+  local revision_by_sha=$(curl -Ls "$latest_snapshot_url" | yq ".spec.components[] | select(.containerImage == \"*$calculated_image\") | .source.git.revision")
+  debug_echo "Revision by SHA: $revision_by_sha"
+
+  local revision_by_repo=$(curl -Ls "$latest_snapshot_url" | yq ".spec.components[] | select(.source.git.url == \"$repo\") | .source.git.revision")
+  debug_echo "Revision by Repo: $revision_by_repo"
+
+  # Return the revision
+  echo "$revision_by_repo"
+}
 
 function print_pr_testability {
 	local pr_url=$1
@@ -125,12 +158,12 @@ function print_pr_testability {
 	local org=$(basename $(dirname $(dirname $(dirname $pr_url))))
     local published_sha=$2
 
-    echo "Testing PR testability of $pr_url"
-    echo "Repo: $repo"
-    echo "Org: $org"
+    debug_echo "Testing PR testability of $pr_url"
+    debug_echo "Repo: $repo"
+    debug_echo "Org: $org"
 
 	local commits="https://api.github.com/repos/$org/$repo/commits"
-    echo "Commits url: $commits"
+    debug_echo "Commits url: $commits"
 	# echo $org $repo $number
 
 	if [[ ! -v repo_commits["$repo"] ]]; then
@@ -158,7 +191,9 @@ function print_pr_testability {
 	# ahead
 	# we only actually care if it's behind or not
 
-	if [ $status == "ahead" ] || [ $status == "identical" ]; then
+	if [ "$status" == "404" ]; then
+		echo "🟨 $org/$repo pull $number has not been merged into the specified branch yet"
+	elif [ $status == "ahead" ] || [ $status == "identical" ]; then
 		echo "🟩 $org/$repo pull $number is in the downstream build"
 	elif [ $status == "behind" ]; then
 		echo "🟥 $org/$repo pull $number is not in the downstream build"
@@ -170,4 +205,11 @@ function print_pr_testability {
 }
 
 
-print_pr_testability $pr $revision_by_repo
+# Process each PR URL
+for pr_url in "${pr_urls[@]}"; do
+  debug_echo ""
+  debug_echo "Processing PR: $pr_url"
+
+  revision=$(get_revision_for_pr "$pr_url")
+  print_pr_testability "$pr_url" "$revision"
+done
